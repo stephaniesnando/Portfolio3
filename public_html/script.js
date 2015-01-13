fc33bc1ac79c417d1bfb5cc0fa12f2d17a64c08d
Stephanie Nando <stephaniesnando@gmail.com>
2015-01-13 11:15:44 -0800

    Day 4, I think

--- public_html/script.js
+++ public_html/script.js
@@ -14,15 +14,33 @@ $("document").ready(function (){
     $("h1").bind("mouseover", mouseOverMe).bind("mouseout", mouseOutMe);
    
     $("h1").bind("click", mouseClick);
+    
+    $('#replaceWText').bind('click', replaceWText);
+    
+    $('#randPara').bind('click', addAPara);
+    
+    $('#removePara').bind('click', removeAPara);
 
 });
+
+function removeAPara(){
+    $('#randPara p:last').remove();
+}
+
+function addAPara(){
+    $('#randPara').append('<p>CooOooOool</p>');
+}
+
+function replaceWText(){
+    $('#replaceWText').text('Bye!');
+}
 
 function alertButtonClick(){
     alert("You clicked my button")
 }
 
 function mouseOverMe(){
-    $("h1").html("blah blah");
+    $("h1").html("CooOoooOl");
 }
 function  mouseOutMe(){
     $("h1").html("whatever forever");
